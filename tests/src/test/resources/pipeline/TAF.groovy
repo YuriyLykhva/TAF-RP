a0a2bc97acf54844144c297ed7fb68f46b8e5403
YuriyLykhva <67342223+YuriyLykhva@users.noreply.github.com>
2023-06-22 15:53:07 +0300

    Update TAF.groovy

--- tests/src/test/resources/pipeline/TAF.groovy
+++ tests/src/test/resources/pipeline/TAF.groovy
@@ -1,6 +1,13 @@
 package pipeline
 pipeline {
     agent any
+    parameters {
+        booleanParam(name: "TEST_BOOLEAN", defaultValue: true, description: "Sample boolean parameter")
+        string(name: "TEST_STRING", defaultValue: "ssbostan", trim: true, description: "Sample string parameter")
+        text(name: "TEST_TEXT", defaultValue: "Jenkins Pipeline Tutorial", description: "Sample multi-line text parameter")
+        password(name: "TEST_PASSWORD", defaultValue: "SECRET", description: "Sample password parameter")
+        choice(name: "TEST_CHOICE", choices: ["production", "staging", "development"], description: "Sample multi-choice parameter")
+    }
     triggers {
         cron('H 8 * * *')
     }
